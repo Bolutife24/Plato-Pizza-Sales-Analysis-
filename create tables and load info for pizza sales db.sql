440f267d7e27fc36aa566121223d83555d241cdb
Bolutife24 <130703090+Bolutife24@users.noreply.github.com>
2023-05-12 21:08:43 +0300

    Add files via upload

---
+++ create tables and load info for pizza sales db.sql
@@ -0,0 +1,82 @@
+USE Portfolio
+-- create tables in the database
+
+
+CREATE TABLE pizza (
+    pizza_id        VARCHAR(80)        NOT NULL,
+    pizza_type_id   VARCHAR(80)        NOT NULL,
+	size            CHAR(5)            NOT NULL,
+	price           FLOAT              NOT NULL
+  );
+GO
+
+CREATE TABLE pizza_type (
+     pizza_type_id       VARCHAR(50)        NOT NULL,
+     name                VARCHAR(50)        NOT NULL,
+	 category            VARCHAR(50)        NOT NULL,
+	 ingredients         VARCHAR(250)       NOT NULL
+
+); 
+GO
+
+DROP TABLE orders_info
+
+CREATE TABLE orders_info (
+     order_id       INT         NOT NULL,
+     date           DATE      NOT NULL,
+	 time           VARCHAR(8)   NOT NULL,
+	PRIMARY KEY(order_id)
+); 
+GO
+
+CREATE TABLE order_details_info (
+     order_details_id       INT               NOT NULL,
+	 order_id               INT               NOT NULL,
+     pizza_id               VARCHAR(50)       NOT NULL,
+	 quantity               INT               NOT NULL,
+	PRIMARY KEY(order_details_id)
+); 
+GO
+
+--Load tables in the database
+
+-- import the file
+BULK INSERT  dbo.pizza
+FROM 'C:\Users\bolutife\OneDrive - University of Eastern Finland\Personal projects\pizzas_2.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+
+BULK INSERT  dbo.pizza_type
+FROM 'C:\Users\bolutife\OneDrive - University of Eastern Finland\Personal projects\pizza_types_2.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+
+BULK INSERT  orders_info
+FROM 'C:\Users\bolutife\OneDrive - University of Eastern Finland\Personal projects\orders_2.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+
+
+BULK INSERT  order_details_info
+FROM 'C:\Users\bolutife\OneDrive - University of Eastern Finland\Personal projects\order_details_2.csv'
+WITH
+(
+        FORMAT='CSV',
+        FIRSTROW=2
+)
+GO
+
+SELECT TOP 10*
+FROM orders_info
